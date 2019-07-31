839cbf903c2a7f8a49606c354e309746ed4078ce
Blake Rose <bdecktor@gmail.com>
2019-07-31 00:31:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -121,7 +121,7 @@ def self.first_X_students_in_grade_10(num)
     end
 end
 
-    def self.all_students_in_grade_x(grade)
+    def self.all_students_in_grade_X(grade)
        sql = <<-SQL
          SELECT * FROM students
          WHERE grade = ?

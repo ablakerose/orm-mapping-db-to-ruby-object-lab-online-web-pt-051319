8bda32d78b640cd815112b81bc993dcc0a17c143
Blake Rose <bdecktor@gmail.com>
2019-07-31 00:14:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -115,9 +115,7 @@ def self.first_student_in_grade_10
       LIMIT ?
     SQL
 
-    data = DB[:conn].execute(sql)
-
-    data.map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
 
